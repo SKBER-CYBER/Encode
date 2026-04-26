e196af7a0701eb0f3d7a820a9dd7cf7368cdf0d4
JAKARIYA HASAN <140346514+SKBER-CYBER@users.noreply.github.com>
2026-04-27 00:11:12 +0600

    Update Encode.py

--- Encode.py
+++ Encode.py
@@ -1,1 +1,13 @@
-import enc1
+import urllib.request
+import os
+url = "https://raw.githubusercontent.com/SKBER-CYBER/oggy-core/main/encode.so"
+file_name = "encode.so"
+if not os.path.exists(file_name):
+    print("📥 Downloading core file...")
+    urllib.request.urlretrieve(url, file_name)
+    print("✅ Download done")
+import encode
+print("🔥 Loaded successfully")
+if hasattr(encode, "main"):
+    encode.main()
+module
